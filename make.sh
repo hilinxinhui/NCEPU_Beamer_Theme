8985da24d1f001fe9a43d8d096dbb15752d1cf9e
linxinhui <hilinxinhui@gmail.com>
2023-06-11 18:58:54 +0800

    Update slide.

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
-xelatex NCEPU_Beamer_Theme
-xelatex NCEPU_Beamer_Theme
+xelatex NCEPU_Beamer_Theme_formatted & 
+xelatex NCEPU_Beamer_Theme_formatted
+xelatex NCEPU_Beamer_Theme_noted & 
+xelatex NCEPU_Beamer_Theme_noted
 rm -f *.aux *.bak *.log *.bbl *.dvi *.blg *.thm *.toc *.out *.lof *.lol *.lot *.nav *.snm *.fdb_latexmk *.synctex.gz
